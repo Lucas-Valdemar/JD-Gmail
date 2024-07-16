36004b580a5b99057a3f8a9d59646e29fc0534e1
Sadn0x <sidneybragacf@outlook.com>
2024-07-15 22:39:56 -0300

    Material Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import AppsIcon from '@mui/icons-material/Apps';
 
 function App() {
 
   return (
     <>
       <h1 className="font-bold">Hello World</h1>
+      <AppsIcon color='error'/>
     </>
   )
 }
